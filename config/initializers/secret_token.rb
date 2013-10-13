ed7cc05077238919d819d7ae17e681ed038a8d8a
akshaychauhan <akshay.chauhan36@gmail.com>
2013-10-13 21:23:37 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fourds::Application.config.secret_key_base = 'f2007f20ca56d86dbc2814d59a8cd7b51426aec6fd7e4d1b71708f723d55dc39e0f15f1bb1b6735a76c882f6ea8c6aa606e8375e275c537588d574d8bfb16e0e'
