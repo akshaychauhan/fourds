95569e8ed122fb5a1f9d84006d780570a2dc8c3f
Akshay Chauhan <akshay.chauhan36@gmail.com>
2016-10-15 14:25:48 +0530

    modify user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+
+  def get_user
+  end
 end
